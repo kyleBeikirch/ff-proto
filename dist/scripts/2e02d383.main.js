bf9f558dcfad02e3701d57c62d1f1a27c2317741
Kyle Beikirch <kyle.beikich@gmail.com>
2013-11-13 15:44:24 -0800

    Build

--- dist/scripts/2e02d383.main.js
+++
@@ -1,1 +0,0 @@
-window.ffProto={Models:{},Collections:{},Views:{},Routers:{},init:function(){"use strict";console.log("Hello from Backbone!")}},$(document).ready(function(){"use strict";ffProto.init()}),this.JST=this.JST||{};
