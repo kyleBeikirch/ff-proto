9a220ac725c4b2d2382b5c9ccfda127e0f95b6e6
Kyle Beikirch <kyle.beikich@gmail.com>
2013-11-13 14:54:30 -0800

    Heroku node to work yeoman

---
+++ dist/scripts/2e02d383.main.js
@@ -0,0 +1,1 @@
+window.ffProto={Models:{},Collections:{},Views:{},Routers:{},init:function(){"use strict";console.log("Hello from Backbone!")}},$(document).ready(function(){"use strict";ffProto.init()}),this.JST=this.JST||{};
