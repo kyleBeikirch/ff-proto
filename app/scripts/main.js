8f10c2f4ad6cac375457c5094a9901077d508f4d
Kyle Beikirch <kyle.beikich@gmail.com>
2013-11-13 17:17:43 -0800

    jsonp returns 404

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,20 +9,28 @@ window.ffProto = {
     init: function () {
         'use strict';
         console.log('Hello from Backbone!');
+
         $.ajax({
-        type: "POST",
-        processData: true,
-        data: {},
-        dataType: "json",
-        url: "https://www.strava.com/oauth/token?client_id=48&client_secret=2776c1edb8022ecef401bb0cb92a3c882dc393b7&code=811733fcb8a5d8d3a6f5a78597e92043329790f0",
-        success: function(data)
-        {
-            console.log(data);
-        },
-        error: function(e)
-        {
-            console.log(e);
-        }
+            type: "POST",
+            contentType: "application/json; charset=utf-8",
+            jsonp : false,
+            cache: true,
+            data: {
+                'client_id' : '48',
+                'client_secret' : '2776c1edb8022ecef401bb0cb92a3c882dc393b7',
+                'code' : '811733fcb8a5d8d3a6f5a78597e92043329790f0',
+            },
+            dataType: "jsonp",
+            jsonpCallback: 'myFunction',
+            url: "https://www.strava.com/oauth/token",
+            success: function(data)
+            {
+                console.log(data);
+            },
+            error: function(e)
+            {
+                console.log(e);
+            }
         });
     }
 };
