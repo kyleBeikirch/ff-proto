4a0e9305feca61b1a841e4c4f23850fc46f866d5
Kyle Beikirch <kyle.beikirch@gmail.com>
2013-12-04 14:46:46 -0800

    Save riders models

--- app/scripts/collections/riders.js
+++ app/scripts/collections/riders.js
@@ -16,7 +16,10 @@ app.Riders = Backbone.Collection.extend({
     	return 'https://www.strava.com/api/v3/clubs/' + app.credentials.get('club_id') + '/members?access_token=' + app.credentials.get('access_token') + '&per_page=200'
     },
     parse: function(response) {
-        console.log(response)
+        var that = this;
+        _(response).each(function(item) {
+            that.add( item );
+        });
         return response.results;
     }
 
